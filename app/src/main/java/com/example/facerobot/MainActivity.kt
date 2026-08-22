1e7cffff508475b4d3cc31550fa61e0152568b00
rinceghrio-del <rinceghrio@gmail.com>
2026-08-22 10:11:18 +0800

    Update ESP32 base URL for connectivity

--- app/src/main/java/com/example/facerobot/MainActivity.kt
+++ app/src/main/java/com/example/facerobot/MainActivity.kt
@@ -79,7 +79,7 @@ class MainActivity : ComponentActivity() {
 
     private var appState = AppState.EYES
 
-    private val esp32BaseUrl = "http://192.168.1.25"
+    private val esp32BaseUrl = "http://10.175.14.169"
 
     private var lastSendTime = 0L
     private val sendIntervalMs = 300L
